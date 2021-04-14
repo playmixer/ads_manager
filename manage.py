5879ba244cf33e072d6c07b4555bf98e565fa9b5
shkirya.ns <shkirya.ns@oilpc.ru>
2021-04-14 16:44:36 +0600

    upd manage

--- manage.py
+++ manage.py
@@ -1,6 +1,8 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from main import create_app, db
+from shutil import copyfile
+import os
 
 app = create_app()
 migrate = Migrate(app, db)
@@ -11,7 +13,10 @@ manager.add_command('db', MigrateCommand)
 
 @manager.command
 def install():
-    print('install')
+    if not os.path.exists('config.py'):
+        copyfile('config.example.py', 'config.py')
+        print('created config.py')
+    print('installed')
 
 
 if __name__ == '__main__':
