af85a404a36fedec35ed82de24050fe9f7e8909c
shkirya.ns <shkirya.ns@oilpc.ru>
2021-04-14 17:12:52 +0600

    config.example.py upd

--- config.example.py
+++ config.example.py
@@ -1,10 +1,20 @@
 from datetime import timedelta
 import logging
 
+# Db
 sql_user = 'root'
 sql_pass = 'root'
+
+# Logger
 LOGGER_LEVEL = logging.INFO
 
+# Gunicorn
+HOST = '127.0.0.1'
+PORT = '8000'
+WORKERS = 3
+WORKER_TIMEOUT = 30
+
+# Flask
 SQLALCHEMY_DATABASE_URI = f'mysql://{sql_user}:{sql_pass}@localhost/advertise'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SECRET_KEY = 'secret_key_for_secure'
